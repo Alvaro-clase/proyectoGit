1d9598529899288987521db3e99bd9a02f10a61f
Alvaro Carrion <alvarocarrion.alu@iespacomolla.es>
2024-11-18 16:34:22 +0100

    Commit de EjemploJava.java y hola.c

---
+++ EjemploJava.java
@@ -0,0 +1,5 @@
+public class EjemploJava {
+    public static void main(String[] args){
+        System.out.println("Hello People!!!!");
+    }
+}
---
+++ hola.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    printf("Hola, mi nombre es: Álvaro y soy alumno de primero de DAW.");
+    return 0;
+
+}
